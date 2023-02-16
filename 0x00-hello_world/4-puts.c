bf9032b47e3b4dcd89770d1a81743900baecb2f9
Osajele Odegua <odeguaj@gmail.com>
2023-02-16 07:48:28 -0800

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio>
+/*
+  * main - print a code using puts
+  *
+  * Return : 0
+*/
+
+ int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	Return (0)
+}
