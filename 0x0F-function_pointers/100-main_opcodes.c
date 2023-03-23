671dfab5754e57193ca9dd1d415ce719006548f9
Osajele Odegua <odeguaj@gmail.com>
2023-03-23 13:20:27 -0700

    function_pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * print_opcodes - print the opcodes of this program
  * @a: address of the main function
  * @n: number of bytes to print
+ *
  * Return: void
  */
-
 void print_opcodes(char *a, int n)
 {
 	int i;
@@ -19,26 +19,26 @@ void print_opcodes(char *a, int n)
 			printf(" ");
 	}
 	printf("\n");
+
 }
 
 /**
- * main - prints the opcodes
- * @argc: number of arguments passed
+ * main - prints the opcodes of its own main function
+ * @argc: number of arguments passed to the function
  * @argv: array of pointers to arguments
- * Return: 0
+ *
+ * Return: always O
  */
-
 int main(int argc, char **argv)
 {
 	int n;
-	int i;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	n = atoi(argv[i]);
+	n = atoi(argv[1]);
 	if (n < 0)
 	{
 		printf("Error\n");
