0189c5c1f3e1934b75c46ba3d5993cdad04aa4d3
Odegua Osajele <odeguaj@gmail.com>
2023-04-04 04:14:54 -0700

    more_singly_linked_lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
-	new_noe->n = n;
+	new_node->n = n;
 	if (idx == 0)
 	{
 		new_node->next = *head;
