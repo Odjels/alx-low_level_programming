3df9dd133472f9ce06938b1950ec41fe893abed7
Odegua Osajele <odeguaj@gmail.com>
2023-04-04 03:42:47 -0700

    4-free_listint.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,14 +10,14 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *next_node;
+	listint_t *next;
 
 	if (head == NULL)
 		return;
 	while (*head != NULL)
 	{
-		next_node = (*head)->next_node;
+		next = (*head)->next;
 		free(*head);
-		*head = next_node;
+		*head = next;
 	}
 }
