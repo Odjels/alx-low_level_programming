db36040121fb1ce1f684ff7cdbd6ea9c4c1a8bf5
Odegua Osajele <odeguaj@gmail.com>
2023-04-04 01:38:20 -0700

    2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -5,22 +5,19 @@
  * @head: pointers to the head of the list
  * @n: integers to be used as content
  *
- * Return:The  address of the newly added node
+ * Return: The address of the newly added node
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new_node;
+	listint_t *ptr;
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node != NULL)
-	{
-		new_node->n = n;
-		new_node->next = *head;
-	}
-	else
+	if (head == NULL)
 		return (NULL);
-	if (*head != NULL)
-		new_node->next = *head;
-	*head = new_node;
-	return (new_node);
+	ptr = malloc(sizeof(listint_t));
+	if (ptr == NULL)
+		return (NULL);
+	ptr->n = n;
+	ptr->next = *head;
+	*head = ptr;
+	return (ptr);
 }
