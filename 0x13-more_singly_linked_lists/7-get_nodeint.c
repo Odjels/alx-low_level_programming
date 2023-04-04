68ae92c5b009be6949177d9a5c48f0e95ee67cf5
Odegua Osajele <odeguaj@gmail.com>
2023-04-04 03:30:33 -0700

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -15,7 +15,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		return (NULL);
 	for (i = 0; i < index; i++)
 	{
-		head = head->next_node;
+		head = head->next;
 		if (head == NULL)
 			return (NULL);
 	}
