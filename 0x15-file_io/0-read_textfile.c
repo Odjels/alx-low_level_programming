96e3ccf30e7fb1b1ce0523b2ceae8adf96a8f45e
Odegua Osajele <odeguaj@gmail.com>
2023-04-06 14:13:35 -0700

    0x15-file_io

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - Reads the text file and prints it to the POSIX stdout.
+ * @filename: The name of the file.
+ * @letters: The numbers of letters to  read and print.
+ *
+ * Return: the actual number of letters it could read and print.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t i, j, k;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	i = open(filename, O_RDONLY);
+	j = read(o, buffer, letters);
+	k = write(STDOUT_FILENO, buffer, j;
+
+	if (i == -1 || j == -1 || k == -1 || k != j)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(o);
+
+	return (k);
+}
